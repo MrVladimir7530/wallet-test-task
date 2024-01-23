aa068fbb5ebb5ac9f9d1d2d3edd799e1a31d9c9c
MrVladimir <vladimir.volkov-2013@yandex.ru>
2024-01-23 09:01:44 +0300

    begin project

---
+++ src/main/java/com/example/wallettesttask/WalletTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallettesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wallettesttask/WalletTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallettesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
